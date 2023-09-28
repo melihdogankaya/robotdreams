6103993e8cc8e74d07952858f5c784e5d3d63e33
Melih Dogankaya <m_dogankaya@hotmail.com>
2023-09-28 19:58:29 +0300

    polymorphism 1

---
+++ RobotDreams.API/Model/Basket.cs
@@ -0,0 +1,24 @@
+﻿namespace RobotDreams.API.Model
+{
+    public class Basket
+    {
+        private List<Product> products = new();
+
+        public double TotalPrice()
+        {
+            double totalPrice = 0;
+
+            foreach (Product product in products)
+            {
+                totalPrice += product.AddKdv();
+            }
+            
+            return totalPrice;
+        }
+
+        public void Add(Product product)
+        {
+            products.Add(product);
+        }
+    }
+}
---
+++ RobotDreams.API/Model/Bread.cs
@@ -0,0 +1,16 @@
+﻿namespace RobotDreams.API.Model
+{
+    public class Bread : Product
+    {
+        public string Type { get; set; }
+        public int BasisWeight { get; set; }
+
+        public Bread(string name, double price, string type, int basisWeight) 
+        { 
+            Name = name;
+            Price = price;
+            Type = type;
+            BasisWeight = basisWeight;
+        }
+    }
+}
