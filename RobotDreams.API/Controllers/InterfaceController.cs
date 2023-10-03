ad8617c24a7ff7b91cd56c754bed740b963a8c49
Melih Dogankaya <m_dogankaya@hotmail.com>
2023-10-03 20:32:41 +0300

    interface örnekleri

---
+++ RobotDreams.API/Controllers/InterfaceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RobotDreams.API.Model.Interface;
+
+namespace RobotDreams.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterfaceController : ControllerBase
+    {
+        [HttpGet]
+        [Route("example1")]
+        public IActionResult InterfaceExample()
+        {
+            Player player = new() { Name = "Lucifer", Age = 30, Weapon = new M51()};
+            return Ok(player.TakeAim());
+        }
+    }
+}
