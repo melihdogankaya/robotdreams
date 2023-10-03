35d2e10dfeefe50f7c7435f4e4d6cd64de9ac8bd
rithmAlgor <h.onurdemir@hotmail.com>
2023-10-03 11:48:40 +0300

    Degisiklik yaptim

--- RobotDreams.API/Controllers/StudiesController.cs
+++ RobotDreams.API/Controllers/StudiesController.cs
@@ -10,28 +10,7 @@ namespace RobotDreams.API.Controllers
     [ApiController]
     public class StudiesController : ControllerBase
     {
-        [HttpGet]
-        [Route("polymorphism")]
-        public IActionResult polymorphism()
-        {
-            Avarage avarage = new();
-
-            FirstClassStudent _FirstClassStudent = new( "Onur", "Demir", 101, 45, 87);
-            SecondClassStudent _SecondClassStudent = new("Ayse", "Aslan", 245, 80, 90, 60, 70);
-            
-            avarage.Add(_FirstClassStudent);
-            avarage.Add(_SecondClassStudent);
-
-            avarage._Avarage = avarage.SchoolAvarage();
-            
-
-            string AvarageSerialized = JsonConvert.SerializeObject(avarage);
-
-            return Ok(AvarageSerialized);
-
-
-
-        }
+        
 
 
 
