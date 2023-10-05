469726d8cdc25259ea48d7d23b0f17227417c683
Mehmetasker <31123405+Aksaka7@users.noreply.github.com>
2023-10-05 19:42:29 +0200

    Cordition

---
+++ RobotDreams.API/Controllers/ConditionController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RobotDreams.API.Controllers
+{
+    public class ConditionController : Controller
+    {
+        [HttpGet]
+        [Route("ifelse")]
+
+        public IActionResult Conditionexample(string name)
+        {
+            string result = string.Empty;
+            if (name.StartsWith("m"))
+            {
+                result = "İsminiz M harfi ile başlıyorsunuz.";
+            }
+            else if (name.StartsWith("M"))
+            {
+                result = "İsminiz M harfi ile başlıyor.";
+            }
+            else
+            {
+                result = "İsminiz m harfi ile başlamıyor.";
+            }
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("switch")]
+
+        public IActionResult Conditionexample2(string teamCode)
+        {
+            //switch (switch_on)
+            //{
+            //   case kontrol1:
+            //          işlemler1;
+            //          break;
+            //     default
+            //     break;     
+            //}
+
+            string result = teamCode switch
+            {
+                "bjk" => "Beşiktaş",
+                "fb" => "Fenerbahçe",
+                "gs" => "Galatasaray",
+                "ts" => "Trabzon",
+                _ => "Hiçbir deger geçerli degil",
+            };
+            return Ok(result);
+        }
+    }
+}
---
+++ RobotDreams.API/Controllers/LoopController.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace RobotDreams.API.Controllers
+{
+    public class LoopController : Controller
+    {
+        [HttpGet]
+        [Route("for")]
+
+        public IActionResult ExampleFor()
+        {
+
+
+            List<int> numbers = new();
+
+            for (int i = 0; i <= 100; i++)
+            {
+                numbers.Add(i);
+            }
+            return Ok(JsonConvert.SerializeObject(numbers));
+        }
+
+        [HttpGet]
+        [Route("for2")]
+
+        public IActionResult ExampleFor2()
+        {
+
+
+            List<int> numbers = new();
+
+            for (int i = 0; i <= 100; i += 2)
+            {
+                numbers.Add(i);
+            }
+            return Ok(JsonConvert.SerializeObject(numbers));
+        }
+
+        [HttpGet]
+        [Route("while")]
+
+        public IActionResult ExampleWhile()
+        {
+
+            int i = 1;
+            List<int> numbers = new();
+
+            //while(Koşul)
+            //{
+
+            //}
+
+            while (i <= 100)
+            {
+                numbers.Add(i);
+                i++;
+            }
+            return Ok(JsonConvert.SerializeObject(numbers));
+        }
+
+        [HttpGet]
+        [Route("foreach")]
+
+        public IActionResult ExampleForeach()
+        {
+            List<int> result = new();
+            //foreach(data_type var_name in collection_var)
+            //{
+
+            //}
+
+            int[] list = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //for(int i = 0; i < list.Length; i++)
+            ////{
+            //    result.Add(list[i]);
+            //}
+
+            foreach (var i in list)
+            {
+                result.Add(i);
+            }
+
+            return Ok(JsonConvert.SerializeObject(result));
+        }
+
+        [HttpGet]
+        [Route("dowhile")]
+
+        public IActionResult ExampleDoWhile()
+        {
+            int x = 21;
+            int y = 21;
+
+            List<int> result = new();
+            do
+            {
+                result.Add(x);
+            } while (x < 20);
+
+
+            //do
+            //{
+            //} while (Koşul);
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("Break")]
+        public IActionResult ExampleBreak()
+        {
+            List<int> result = new();
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                result.Add(i);
+            }
+
+            return Ok(JsonConvert.SerializeObject(result));
+        }
+
+        [HttpGet]
+        [Route("Continues")]
+        public IActionResult ExampleContinues()
+        {
+            List<int> result = new();
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i == 5)
+                {
+                    continue;
+                }
+                result.Add(i);
+            }
+
+            return Ok(JsonConvert.SerializeObject(result));
+        }
+    }
+}
