25edc530deb59fe76333b16d0912b681becc2944
rithmAlgor <h.onurdemir@hotmail.com>
2023-10-10 10:55:48 +0300

    we

--- RobotDreams.API/Model/Studies/Abst/Drawing.cs
+++ RobotDreams.API/Model/Studies/Abst/Drawing.cs
@@ -4,6 +4,7 @@
     {
         public string @System { get; set; }
         public string Description { get; set; }
+        public string HowToDraw { get; set; } 
 
         public abstract string Draw();
 
