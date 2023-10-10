658c520bd22b2f08f3d8ba92d94540b47b2ed3df
rithmAlgor <h.onurdemir@hotmail.com>
2023-10-10 10:33:48 +0300

    abs

--- RobotDreams.API/Model/Studies/Abst/Drawing.cs
+++ RobotDreams.API/Model/Studies/Abst/Drawing.cs
@@ -2,6 +2,9 @@
 {
     public abstract class Drawing
     {
+        public string @System { get; set; }
+        public string Description { get; set; }
+
         public abstract string Draw();
 
     }
