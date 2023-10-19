c7ae428ec2bbe562a843551d57ea9203850049fa
rithmAlgor <h.onurdemir@hotmail.com>
2023-10-19 15:22:34 +0300

    data

---
+++ RobotDreams.API/Controllers/DateTimeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RobotDreams.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DateTimeController : ControllerBase
+    {
+    }
+}
