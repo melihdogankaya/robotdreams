6f26007a5c6b2f7ef3445079a9b456e203966d7b
rithmAlgor <h.onurdemir@hotmail.com>
2023-10-26 16:35:22 +0300

    son degisiklikler

--- RobotDreams.API/Controllers/DateTimeController.cs
+++ RobotDreams.API/Controllers/DateTimeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Globalization;
 
 namespace RobotDreams.API.Controllers
@@ -12,9 +13,10 @@ namespace RobotDreams.API.Controllers
         [Route("Date to string")]
         public IActionResult Example1()
         {
-            CultureInfo[] culture = new CultureInfo[] 
+            List<string> result = new List<string>();
+            CultureInfo[] culture = new CultureInfo[]
             {
-                CultureInfo.InvariantCulture, 
+                CultureInfo.InvariantCulture,
                 new CultureInfo("en-US"),
                 new CultureInfo("fr-fr"),
                 new CultureInfo("de-de"),
@@ -23,13 +25,52 @@ namespace RobotDreams.API.Controllers
             };
 
             DateTime now = DateTime.Now;
-            foreach (var cultureInfo in culture) 
+            foreach (var cultureInfo in culture)
             {
+                string cultureName;
+                if (string.IsNullOrEmpty(cultureInfo.NativeName))
+                {
+                    cultureName = cultureInfo.Name;
+                }
+                else
+                {
+                    cultureName = cultureInfo.Name;
+                }
+                result.Add($"in {cultureName}, {now.ToString(cultureInfo)}");
+            }
+            return Ok(JsonConvert.SerializeObject(result));
+        }
 
-
+        [HttpGet]
+        [Route("Date to string 2")]
+        public IActionResult Example2()
+        {
+            List<string> result = new();
+            var date = new DateTime(2000, 03, 19, 18, 3,12);
+            string[] standardFnts = { "d", "D", "f", "F", "g", "G", "m", "o" };
+            foreach (var item in standardFnts)
+            {
+                result.Add($"{item} : {date.ToString(item)}");  
             }
 
-            return Ok();
+            return Ok(JsonConvert.SerializeObject(result));
+        }
+
+        [HttpGet]
+        [Route("Parse DateTime")]
+        public IActionResult ParseDateTime()
+        {
+
+            var stringDate = "13/10/2023 19:27";
+           var result = DateTime.TryParse(stringDate, out DateTime date);
+           
+            if(result)
+            {
+               date = date.AddDays(4);
+            }
+            
+            return Ok(date);
         }
     }
+
 }
