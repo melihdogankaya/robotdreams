05e9778965162fe5378e002390ef6328a8102ddb
rithmAlgor <h.onurdemir@hotmail.com>
2023-10-31 16:27:01 +0300

    q

--- RobotDreams.API/Program.cs
+++ RobotDreams.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<RobotDreamsDbContext>(options => options.UseSqlServer(connection));
 
 var app = builder.Build();
 
