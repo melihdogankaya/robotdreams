60236d2aab0b0e282206523345122320242881f8
Cemal Armağan TOPAL <catopal@outlook.com>
2023-10-31 11:52:59 +0300

    c

---
+++ RobotDreams.API/Controllers/Homework01Controller.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RobotDreams.API.Model.GenericType;
+
+namespace RobotDreams.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Homework01Controller : ControllerBase
+    {
+        [HttpGet]
+        [Route("Exceptions")]
+        public IActionResult GenericTypeAndExceptions(string input1, string? input2=null)
+        {
+            Homework01Generic<int> homework01Generic = new Homework01Generic<int>();
+
+            try
+            {
+                string test = input2.GetType().Name;
+                int number1 = Convert.ToInt32(input1);
+                int number2 = Convert.ToInt32(input2);
+                
+
+                homework01Generic.Result = number1 * number2;
+                homework01Generic.Message = "Input 1 multiply by Input 2 = ";
+
+                return Ok(homework01Generic.Message + homework01Generic.Result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.GetType().Name);
+            }
+
+
+
+        }
+    }
+}
---
+++ RobotDreams.API/Model/GenericType/Homework01Generic.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace RobotDreams.API.Model.GenericType
+{
+    public class Homework01Generic<T>
+    {
+        public T Result { get; set; }
+        public string Message {get; set;}
+    }
+}
