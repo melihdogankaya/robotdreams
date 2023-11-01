b665abb36daa24713f5761f9318525d78187400c
rithmAlgor <h.onurdemir@hotmail.com>
2023-11-01 17:00:35 +0300

    f

--- RobotDreams.API/Context/Domain/User.cs
+++ RobotDreams.API/Context/Domain/User.cs
@@ -2,5 +2,11 @@
 {
     public class User
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string surname { get; set; }
+
+        public string Email { get; set; }   
+        public string Password { get; set; }
     }
 }
--- RobotDreams.API/Context/RobotDreamsDbContext.cs
+++ RobotDreams.API/Context/RobotDreamsDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RobotDreams.API.Context.Domain;
 
 namespace RobotDreams.API.Context
 {
